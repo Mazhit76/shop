70ccd33e83041ffaeccc9968fc02b0c3b2c73a38
Mazhit 76 <mig7676@yandex.ru>
2021-02-11 12:32:59 +0400

    HW-4 to Lector for check

--- static/js/orders_script.js
+++ static/js/orders_script.js
@@ -19,4 +19,72 @@ window.onload = function () {
         }
     }
 
+
+
+    $('.order_form').on('click', 'input[type="number"]', function () {
+        var target = event.target;
+        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-quantity', ''));
+        if (price_arr[orderitem_num]) {
+            orderitem_quantity = parseInt(target.value);
+            delta_quantity = orderitem_quantity - quantity_arr[orderitem_num];
+            quantity_arr[orderitem_num] = orderitem_quantity;
+            orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+        }
+    });
+
+    $('.order_form').on('click', 'input[type="checkbox"]', function () {
+        var target = event.target;
+        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
+        if (target.checked) {
+            delta_quantity = -quantity_arr[orderitem_num];
+        } else {
+            delta_quantity = quantity_arr[orderitem_num];
+        }
+        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    });
+    //Для корректного обновления стоимости при удалении элементов заказа, вместо предыдущего обработчика
+    // пропиcываем код функции «deleteOrderItem»:
+    //
+    // $('.order_form').on('click', 'input[type="checkbox"]', function deleteOrderItem(row) {
+    //     var target_name= row[0].querySelector('input[type="number"]').name;
+    //     orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
+    //     delta_quantity = -quantity_arr[orderitem_num];
+    //     orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    //     });
+
+    function orderSummaryUpdate(orderitem_price, delta_quantity) {
+        delta_cost = orderitem_price * delta_quantity;
+
+        order_total_cost = Number((order_total_cost + delta_cost).toFixed(2));
+        order_total_quantity = order_total_quantity + delta_quantity;
+
+        $('.order_total_cost').html(order_total_cost.toString());
+        $('.order_total_quantity').html(order_total_quantity.toString());
+    }
+
+    //Добавление новых форм к набору при помощи django-dynamic-formset
+
+    $('.formset_row').formset({
+        addText: 'добавить продукт',
+        deleteText: 'удалить',
+        prefix: 'orderitems',
+        removed: deleteOrderItem
+    });
+
+    //Если понадобится корректно обновлять статистику при добавлении новых элементов в заказ
+    // - можем создать еще один обработчик для изменения значения элементов «select»
+
+    $('.order_form select').change(function () {
+        var target = event.target;
+        console.log(target);
+    });
+
+    if (!order_total_quantity) {
+        for (var i=0; i < TOTAL_FORMS; i++) {
+            order_total_quantity += quantity_arr[i];
+            order_total_cost += quantity_arr[i] * price_arr[i];}
+        $('.order_total_quantity').html(order_total_quantity.toString());
+        $('.order_total_cost').html(Number(order_total_cost.toFixed(2)).toString());
+        }
+
 }
